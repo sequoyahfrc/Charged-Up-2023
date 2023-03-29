17021b08f5057d9b4aee1c83bbba9c4bf24f0bd4
Sequoyah PCs <sequoyahpcs@sequoyahrobotics.com>
2023-03-29 18:04:10 -0400

    added command to set angle wrist with PID

---
+++ src/main/java/frc/robot/claw/commands/SetClawAngleCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.claw.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.claw.ClawConstants;
+import frc.robot.claw.ClawSubsystem;
+
+public final class SetClawAngleCommand extends CommandBase {
+
+    private final ClawSubsystem clawSubsystem;
+    private final PIDController pid = new PIDController(ClawConstants.WRIST_P, 0, 0);
+
+    public SetClawAngleCommand(double angle, ClawSubsystem clawSubsystem) {
+        this.clawSubsystem = clawSubsystem;
+        pid.setTolerance(ClawConstants.WRIST_ERROR);
+        pid.setSetpoint(angle);
+        addRequirements(clawSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        clawSubsystem.setWrist(pid.calculate(clawSubsystem.getAngle()));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return pid.atSetpoint();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        clawSubsystem.setWrist(0);
+    }
+}
