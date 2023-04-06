78f11cd9899f96020473c0833dd9432c3eef4dd4
Sequoyah PCs <sequoyahpcs@sequoyahrobotics.com>
2023-04-06 16:36:35 -0400

    elevator tuning

--- src/main/java/frc/robot/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/elevator/ElevatorConstants.java
@@ -7,7 +7,7 @@ public final class ElevatorConstants {
     public static final int BOTTOM_LIMIT_SWITCH_ID = 8;
     public static final int TOP_LIMIT_SWITCH_ID = 9;
 
-    public static final double P = 0.1;
+    public static final double P = 0.05;
     public static final double D = 0.0;
     public static final double DEADBAND = 0.05;
     public static final double KS = 0.1;
@@ -20,7 +20,7 @@ public final class ElevatorConstants {
 
     public static final double SUBSTATION_HEIGHT = 5;
 
-    public static final double ERROR = 0.1;
+    public static final double ERROR = 0.125;
 
     public static final double MAX_HEIGHT = 43.0;
 
